d2cbaeef47cddb2cf64909f69d4e95fa00dff935
Nikita1814 <kravchenko1814@gmail.com>
2021-11-05 22:11:28 +0300

    feat: finish write-file task

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,14 +1,36 @@
 const fs = require("fs");
 const path = require("path");
 const { stdin } = process;
-fs.writeFile(path.join(__dirname, "text.txt"), (err) => {
-  if (err) throw err;
-  connsole.log('The text file has been successfully generated.\nPlease be so kind as to use to input a short textual message in any language utilizing your keyboard, please accept in advance our utmost gratitude for your presumed cooperation')
+const stream = fs.createWriteStream(path.join(__dirname,"text.txt"), "utf-8");
+const rl = require("readline").createInterface({
+    input: process.stdin,
+    output: stream})
+
+    
+console.log(
+  "The text file has been successfully generated.\nPlease be so kind as to use to input a short textual message in any language utilizing your keyboard, please accept in advance our utmost gratitude for your presumed cooperation"
+);
+
+rl.on('line', (input) =>{
+    if(input === 'exit'){
+    rl.close()
+    process.exit()    
+    } else{
+    rl.output.write(`\n${input}`)
+    }   
 })
 
-/*stdin.on('data', data =>{
-    fs.createWriteStream((path.join(__dirname, "text.txt"))).write(data.toString)} )
 
-process.on('exit', () => {
-console.log('Thank you for your cooperation and for the submitted information, we sincerely wish you a joyful and productive day!')
-})*/
+
+rl.on('close' ,()=>{
+console.log('---EXIT MSG---\nThank you for provided information. We wish to express our feelings of profound joy upon the reception of said message and we hope that as we surely will, you too will keep the textual file as a memento of our short yet immeasurably pleasant exchange\n---EXIT MSG---')
+})
+
+rl.on('SIGINT', () =>{
+process.emit("SIGINT")
+})
+
+process.on('SIGINT', ()=>{
+    console.log('---EXIT MSG---\nThank you for provided information. We wish to express our feelings of profound joy upon the reception of said message and we hope that you too will keep the textual file as a memento of our short yet immeasurably pleasant exchange\n---EXIT MSG---')
+    process.exit()
+})
