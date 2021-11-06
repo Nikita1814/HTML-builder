d94308397392597bb425d83701f22ab3ab5820d4
Nikita1814 <kravchenko1814@gmail.com>
2021-11-06 16:57:04 +0300

    feat: add directory copying function to copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,16 @@
+const fs = require("fs");
+const path = require("path");
+const fsPromises = require("fs/promises");
 
+async function copyDir() {
+  await fsPromises.mkdir(path.join(__dirname, `files-copy`), {
+    recursive: true,
+  });
+
+  const files = await fsPromises.readdir(path.join(__dirname, "files"));
+  for (file of files) {
+      console.log(file)
+    fs.copyFile(path.join(__dirname, "files", file), path.join(__dirname, "files-copy", file), ()=>{console.log(`copied ${file} from ${path.join(__dirname, "files", file)}`)});
+  }
+}
+copyDir();
