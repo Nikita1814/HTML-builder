f8a47e9f2e8a112939ce8288613da89762830f8a
Nikita1814 <kravchenko1814@gmail.com>
2021-11-08 21:53:10 +0300

    feat: add css bundling functionality to build-page-task

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -41,7 +41,38 @@ await fsPromises.mkdir(path.join(__dirname, `project-dist`), {
         }
      })
 }
-createDist ()
 
-  copyDir(path.join(__dirname, "assets"))
+async function bundleFiles() {
+    const files = await fsPromises.readdir(
+      path.join(__dirname, "styles"),
+      { withFileTypes: true }
+    );
+    let bundle = []
+    for (file of files) {
+    if (file.isFile() && path.parse(file.name).ext ==='.css' ) {
+     let res = await fsPromises.readFile(path.join(__dirname, "styles",file.name), 'utf-8', function( err, data){
+     if(err) console.log(err)
+    return data
+     /*console.log(data)*/
+     }
+    )
+    bundle.push(res)
+      }
+    }
+    console.log(bundle.length)
+
+    fs.writeFile(path.join(__dirname,'project-dist','bundle.css'), bundle.join(''), function( err){
+    if (err) console.log(err)
+    console.log(`file bundle.css created on path ${path.join(__dirname,'project-dist','bundle.css')}`)
+    } )
+}
+
+
+
+
+
+createDist ()
+copyDir(path.join(__dirname, "assets"))
+bundleFiles()
+
   
