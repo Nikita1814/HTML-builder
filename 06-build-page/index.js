ec146b19487d465b618ba7cd0ee093f2969e5b70
Nikita1814 <kravchenko1814@gmail.com>
2021-11-08 21:50:54 +0300

    feat: add assets copying functionality to build-page-task

---
+++ 06-build-page/index.js
@@ -0,0 +1,47 @@
+const fs = require("fs");
+const path = require("path");
+const fsPromises = require("fs/promises");
+
+
+
+//making making folder
+async function createDist (){
+await fsPromises.mkdir(path.join(__dirname, `project-dist`), {
+    recursive: true,
+  });
+}
+//copying assets and creating dist
+/*async function copyDir(sourcePath , destinationPath) {
+    const subfolders = await fsPromises.readdir(path.join(__dirname, "assets"));
+    for (subfolder of subfolders) {
+        console.log(file)
+      fs.copyFile(path.join(__dirname, "assets", file), path.join(__dirname, "project-dist", 'assets', file), ()=>{console.log(`copied ${file} from ${path.join(__dirname, "files", file)}`)});
+    }
+  }*/
+
+  async function copyDir(_dir){
+    fs.readdir(_dir, { withFileTypes: true }, function(err, items){
+        if(err) {throw err}
+        console.log('working')
+        for (let i = 0; i < items.length; i++){
+          if (items[i].isDirectory()){
+              console.log(`dir detected ${items[i].name}`)
+              async function d(){
+              await fsPromises.mkdir(path.join(__dirname, `project-dist`, 'assets', items[i].name), {
+                recursive: true,
+              })
+            }
+            d()
+            console.log(path.join(__dirname, `project-dist`, 'assets', items[i].name))
+             copyDir(_dir + '/' + items[i].name)
+          } else if (items[i].isFile()){
+            console.log(`file detected ${items[i].name}`)
+            fs.copyFile(_dir + '/' + items[i].name, `${_dir.split('06-build-page')[0]}` + `06-build-page\\project-dist` + path.resolve(`${_dir.split('06-build-page')[1]}`).substring(2) +  `\\${items[i].name}`, ()=> {console.log(`${items[i].name} --- ${_dir + '/' + items[i].name} --- ${_dir.split('06-build-page')[0]}` + `06-build-page\\project-dist` + `${_dir.split('06-build-page')[1]}` +  `\\${items[i].name}`)})
+          }
+        }
+     })
+}
+createDist ()
+
+  copyDir(path.join(__dirname, "assets"))
+  
